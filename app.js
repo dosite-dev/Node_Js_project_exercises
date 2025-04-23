84e7339ed60eb308f9fb5e31dc8ca3938dd0f3f4
dosite-dev <dositecyiza@gmail.com>
2025-04-23 20:32:46 +0200

    ES module way of importing and exporting files

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
-const {generateRandomNumber,celciusToFahrenheit} = require('./utilis')
-console.log(`Random number:${generateRandomNumber()}`)
-console.log(`Celcius To Fahrenheit:${celciusToFahrenheit(0)}`)
+// const {generateRandomNumber,celciusToFahrenheit} = require('./utilis')
+// console.log(`Random number:${generateRandomNumber()}`)
+// console.log(`Celcius To Fahrenheit:${celciusToFahrenheit(0)}`)
+
+
+import getPosts,{getLengthOfPosts} from './postController.js'
+console.log(getPosts())
+console.log(getLengthOfPosts());
 
---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts =[
+    {id : 1,title : "Post one"},
+    {id : 2,title : "Post Two"},
+
+];
+
+ const getPosts =()=> posts 
+ export const getLengthOfPosts =() => posts.length
+ export default getPosts
+ 
