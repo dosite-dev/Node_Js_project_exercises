a0178c5ba508d51b756a56a67e0685b2ee9c10f6
dosite-dev <dositecyiza@gmail.com>
2025-04-24 22:23:05 +0200

    HTTP module,NPM script,NPM modules,NPM modules and nodemon,req obj,marking request from postman,simple routing

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 import http from 'http'
 const PORT = process.env.PORT
 const server = http.createServer((req,res) =>{
-    // res.setHeader('Content-Type','text/plain')
-    res.writeHead(200,{"Content-Type":"text/html"})
-    res.end("<h1>Hello world</h1>")
+    try {
+        // Check if GET request 
+        if(req.method === 'GET'){
+            if(req.url === '/'){
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.end("<h1>Hello world</h1>")
+            }
+            else if(req.url === '/about'){
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.end("<h1>About</h1>")
+            }
+            res.writeHead(404,{ 'content-type':'text/plain'})
+            res.end('<h1>Not Found</h1>')
+        }
+        else{
+            throw new Error('Method not allowed')
+        }
+        
+    } catch (error){
+        res.writeHead(500,{'content-type':'text/plain'})
+        res.end("Server error")
+        
+    }
+ 
 })
 
 server.listen(PORT,() =>{
